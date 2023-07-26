7309637dd343a549e0f1264849b44007606154ec
Jozuo <jozuo.dev@gmail.com>
2023-07-26 15:26:12 +0900

    CORS設定の追加

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -42,12 +42,14 @@ INSTALLED_APPS = [
     "rest_framework",
     "rest_framework.authtoken",
     "dj_rest_auth",
+    "corsheaders",
     # application
     "apiv1.apps.Apiv1Config",
     "shop.apps.ShopConfig",
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -147,3 +149,9 @@ SIMPLE_JWT = {
     "AUTH_HEADER_TYPES": ("JWT",),
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60),  # アクセストークンの有効期間を60に延長
 }
+
+# CORS
+CORS_ALLOW_ALL_ORIGINS = False
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+]
